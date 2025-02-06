0f739d54f97634c4906436c064a8d485139b323e
Aniket Kumar <143476785+Aniks18@users.noreply.github.com>
2025-02-06 19:16:41 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,7 +10,9 @@ const About = () => {
               <h2 className="text-2xl font-bold text-blue-900">About The Conference</h2>
             </div>
             <p className="text-gray-700 leading-relaxed">
-              The International Conference on Innovations & Recent Trends in Applied Mathematics (ICIRTAM'25) is being organized by the Department of Mathematics on 21st & 22nd of February 2025 at SRM Institute of Science and Technology, Vadapalani campus, Chennai.
+             The International Conference on Innovations & Recent Trends in Applied Mathematics (ICIRTAM'25) is being  organized by the Department of Mathematics on 21st & 22nd of
+February 2025 at SRM Institute of Science and Technology, Vadapalani campus, Chennai. The objective of the conference is to provide a platform for updating the current research and to interact with various scientists and researchers working in the related research fields which will give an opportunity to gain knowledge. The conference includes key note addresses and papers. This conference is the premier forum for the presentation of new advances, research results and to discuss the future trends in the field of mathematics.
+
             </p>
           </div>
  
@@ -21,7 +23,7 @@ const About = () => {
               <h2 className="text-2xl font-bold text-blue-900">About The Department</h2>
             </div>
             <p className="text-gray-700 leading-relaxed">
-              The department offers opportunities for education and research in Mathematics, taking a leading role in bridging the gap between schooling to college using various methodologies.
+              The department offers opportunities for the education and  research in a wide spectrum of areas in Mathematics. The department has been taking a leading role in filling the gap  between the schooling to college by using various methodologies. The department has strongly motivated faculty  with diverse specialization in Mathematical sciences providing a potential for pursuing research in basic sciences as well as in  interdisciplinary areas of science, engineering and technology. The department is committed to train the students to make  them motivated and dedicated engineers and scientists. Our faculty have extensive experience in research as well as teaching.  Our strength is a reflection of their skills, innovation and drive. Our staff members typically have a high sense of purpose and derive  great meaning from working together to ensure SRM Institute of Science and Technology academic and research excellence is  well supported.
             </p>
           </div>
         </div>
